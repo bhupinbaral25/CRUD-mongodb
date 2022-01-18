5917a94808353d1b18bec5b2f497cf42e1317892
bhupin <bhupin@bottle.com.np>
2022-01-18 12:44:02 +0545

    create api completed

---
+++ src/create.py
@@ -0,0 +1,22 @@
+import json
+from flask import response, jsonify
+from model import TODO
+
+def create_todo(event, context):
+    response_body = {}
+    try:
+        data = json.loads(event["body"])
+        todo = TODO(**data).save()
+        response_body = {
+            "data": json.loads(todo.to_json()),
+            "status": True,
+            "msg": "todo created successfully"
+        }
+        status_code = 200
+    except Exception as e:
+        response_body = {
+            "msg": str(e), 
+            "status": False
+        }
+        status_code = 400
+    return response(status_code, jsonify(response_body))
