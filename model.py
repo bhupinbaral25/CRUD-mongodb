ee1cb03c842c2c72f39ed230253db5e669e3eae8
bhupin <bhupin@bottle.com.np>
2022-01-18 12:50:17 +0545

    helper class to connect database

---
+++ model.py
@@ -0,0 +1,42 @@
+import datetime
+import os
+from mongoengine import connect
+import mongoengine_goodjson as gj
+from mongoengine.fields import DateTimeField, StringField
+from mongoengine import ValidationError
+
+#  Read key-value pairs from a .env file and set them as environment variables
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database_name = os.getenv("DATABASE_NAME")
+host = os.getenv("DB_HOST")
+
+# Connecting to MongoDB
+host = f"mongodb+srv://{user}:{password}@{host}/{database_name}"
+connect(host=host)
+
+
+def not_null():
+    raise ValidationError("This field can not be empty")
+
+
+class DefaultAttributes:
+    meta = {"allow_inheritance": True}
+    creation_date = DateTimeField()
+    modified_date = DateTimeField(default=datetime.datetime.now)
+
+    def save(self, *args, **kwargs):
+        if not self.creation_date:
+            self.creation_date = datetime.datetime.now()
+        self.modified_date = datetime.datetime.now()
+        return super(DefaultAttributes, self).save(*args, **kwargs)
+
+    def update(self, *args, **kwargs):
+        self.modified_date = datetime.datetime.now()
+        return super(DefaultAttributes, self).save(*args, **kwargs)
+
+
+class TODO(DefaultAttributes, gj.Document):
+    tittle = StringField(max_length=200, required=True, validation=not_null)
+    description = StringField(required=True)
+    
